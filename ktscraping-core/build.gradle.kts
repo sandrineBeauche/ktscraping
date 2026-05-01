897f9fca717bed4beee18b92bef93f1c24de64b4
sandrineBeauche <sandrine.beauche@gmail.com>
2026-05-01 22:52:36 +0200

    turn ktscraping into multimodules

---
+++ ktscraping-core/build.gradle.kts
@@ -0,0 +1,100 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    application
+    kotlin("plugin.serialization") version "1.8.0"
+    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
+    idea
+}
+
+
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val coroutinesVersion: String by project
+val mockkVersion: String by project
+val kodeinVersion: String by project
+val playwrightVersion: String by project
+val hamkrestVersion: String by project
+val kotlinLoggingVersion: String by project
+val ktorVersion: String by project
+val meercatVersion: String by project
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutinesVersion")
+
+    implementation(kotlin("reflect"))
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+    api ("org.kodein.di:kodein-di:$kodeinVersion")
+    api("org.kodein.di:kodein-di-jvm:$kodeinVersion")
+    api("com.microsoft.playwright:playwright:$playwrightVersion")
+
+    api("it.skrape:skrapeit:1.2.2")
+    implementation("com.fleeksoft.ksoup:ksoup:0.2.1")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+
+    implementation("org.dizitart:nitrite-bom:4.3.0")
+    implementation("org.dizitart:nitrite:4.3.0")
+    implementation("org.dizitart:potassium-nitrite:4.3.0")
+    implementation("org.dizitart:nitrite-mvstore-adapter:4.3.0")
+
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.1")
+    implementation("com.nfeld.jsonpathkt:jsonpathkt:2.0.1")
+
+    implementation("org.sbm4j:meercat:${meercatVersion}")
+    testImplementation(testFixtures("org.sbm4j:meercat:${meercatVersion}"))
+    testImplementation("org.sbm4j:meercat:${meercatVersion}:test-fixtures-sources")
+
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+    testImplementation(kotlin("test"))
+    testImplementation("com.natpryce:hamkrest:$hamkrestVersion")
+    testImplementation("org.sbm4j:meercat:1.1.1")
+
+
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+task("codegen", JavaExec::class) {
+    mainClass = "com.microsoft.playwright.CLI"
+    classpath = sourceSets["main"].runtimeClasspath
+    args = mutableListOf("codegen")
+}
+
+publishing{
+    repositories {
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/sandrineBeauche/ktscraping")
+            credentials{
+                username = System.getenv("GITHUB_PACKAGE_REGISTRY_USER")
+                password = System.getenv("GITHUB_PACKAGE_REGISTRY_TOKEN")
+            }
+        }
+    }
+}
+
+idea {
+    module {
+        isDownloadSources = true
+        isDownloadJavadoc = true
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,4 @@ plugins {
 }
 
 rootProject.name = "ktscraping"
+include("ktscraping-core")
